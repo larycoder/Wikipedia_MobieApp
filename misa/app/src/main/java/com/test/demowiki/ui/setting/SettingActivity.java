0fdc325812215aa8b3f6aaebb4518e262f8a01ed
MidoDoan <huongdl.hx@gmail.com>
2019-11-29 13:01:37 +0700

    Language setting

--- misa/app/src/main/java/com/test/demowiki/ui/setting/SettingActivity.java
+++ misa/app/src/main/java/com/test/demowiki/ui/setting/SettingActivity.java
@@ -2,6 +2,9 @@ package com.test.demowiki.ui.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.test.demowiki.R;
+import com.test.demowiki.ui.language_setting.LanguageSettingActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SettingActivity extends AppCompatActivity {
@@ -11,6 +14,10 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+    }
+    public void onClickLanguage(){
+        Intent startLanguageSetting = new Intent(this, LanguageSettingActivity.class);
+        startActivity(startLanguageSetting);
     }
 
 }
